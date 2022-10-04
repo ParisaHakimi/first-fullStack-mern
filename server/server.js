a635d0efc025d10c96ac99cf11dd275ff89aa940
Parisa Sohrabi <parisahs@gmail.com>
2022-10-04 12:05:16 -0400

    troubleshoot

--- server/server.js
+++ server/server.js
@@ -3,8 +3,8 @@ const cors=require('cors')
 const app=express()
 const PORT=8000
 app.use(cors())
-// const AllpersonRoutes=require('./routes/person.routes')
-// AllpersonRoutes(app)
+const AllpersonRoutes=require('./routes/person.routes')
+AllpersonRoutes(app)
 
 
 app.listen(PORT,console.log(`server is listening on port ${PORT}`))
